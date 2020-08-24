a335518bef7e087dc2bc295182adcb709071d07d
tahsin <amin1996bangladesh>
2020-08-24 09:08:21 -0400

    Working on how to read a csv file

--- src/main/java/com/highchart/trychart/controller/AjaxController.java
+++ src/main/java/com/highchart/trychart/controller/AjaxController.java
@@ -2,9 +2,14 @@ package com.highchart.trychart.controller;
 
 import com.highchart.trychart.modal.BracubBoxScore;
 import com.highchart.trychart.modal.PlayerScore;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -14,6 +19,21 @@ public class AjaxController {
 
     @RequestMapping("/ajax/box_score_result")
     public BracubBoxScore bracubBoxScore(){
+
+        var filename = "src/main/resources/numbers.csv";
+        try(var fr = new FileReader(filename, StandardCharsets.UTF_8);
+            var reader = new CSVReader(fr)){
+            String[] nextLine;
+
+            while((nextLine = reader.readNext()) != null){
+                for (var e: nextLine){
+                    System.out.printf("%s", e);
+                }
+            }
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+
         List<String> players = new ArrayList<String>();
         players.add("Ahnaf Ahmed");
         players.add("Masnun Alvi");
