f4a513f09ecd16fe859fdcbab439ec5fad1a0fa8
tahsin <amin1996bangladesh>
2020-08-25 09:29:37 -0400

    Reading data from a csv file and visualizing the data

--- src/main/java/com/highchart/trychart/controller/AjaxController.java
+++ src/main/java/com/highchart/trychart/controller/AjaxController.java
@@ -12,74 +12,48 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 @RestController
 public class AjaxController {
 
     @RequestMapping("/ajax/box_score_result")
-    public BracubBoxScore bracubBoxScore(){
+    public BracubBoxScore bracubBoxScore() {
+
+        List<PlayerScore> playerScores = new ArrayList<PlayerScore>();
+        BracubBoxScore score = new BracubBoxScore();
+        score.setTitle("BRACU Basketball Players' Box Scores");
 
         var filename = "src/main/resources/numbers.csv";
-        try(var fr = new FileReader(filename, StandardCharsets.UTF_8);
-            var reader = new CSVReader(fr)){
+        try (var fr = new FileReader(filename, StandardCharsets.UTF_8);
+             var reader = new CSVReader(fr)) {
             String[] nextLine;
-
-            while((nextLine = reader.readNext()) != null){
-                for (var e: nextLine){
-                    System.out.printf("%s", e);
+            //
+            List<String> categories = new ArrayList<String>();
+            if ((nextLine = reader.readNext()) != null) {
+                for (String x : nextLine) {
+                    categories.add(x);
                 }
             }
+            categories.remove(0);
+            score.setCategories(categories);
+
+            while ((nextLine = reader.readNext()) != null) {
+                PlayerScore playerScore = new PlayerScore();
+                playerScore.setName(nextLine[0]);
+                List<Integer> playersData = new ArrayList<Integer>();
+
+                for (int i = 1; i <= 5; i++) {
+                    playersData.add(Integer.parseInt(nextLine[i]));
+                }
+
+                playerScore.setData(playersData);
+                playerScores.add(playerScore);
+            }
+            score.setSeries(playerScores);
+
         } catch (IOException | CsvValidationException e) {
             e.printStackTrace();
         }
-
-        List<String> players = new ArrayList<String>();
-        players.add("Ahnaf Ahmed");
-        players.add("Masnun Alvi");
-        players.add("Sakib Anjum");
-        players.add("Zubair Islam");
-
-        List<PlayerScore> playerScores = new ArrayList<PlayerScore>();
-
-        for (String player : players){
-            PlayerScore playerScore = new PlayerScore();
-            playerScore.setName(player);
-            playerScore.setData(buildPlayersData());
-
-            playerScores.add(playerScore);
-        }
-
-        BracubBoxScore score = new BracubBoxScore();
-
-        score.setTitle("BRACU Basketball Players' Box Scores");
-        score.setCategories(buildPointsCategories());
-        score.setSeries(playerScores);
-
         return score;
-    }
-
-    private List<String> buildPointsCategories() {
-        List<String> categories = new ArrayList<String>();
-
-        categories.add("PTS");
-        categories.add("AST");
-        categories.add("TOV");
-        categories.add("STL");
-        categories.add("FG%");
-
-        return categories;
-    }
-
-    private List<Integer> buildPlayersData() {
-        Random rnd = new Random();
-
-        List<Integer> playersData = new ArrayList<Integer>();
-
-        for (int i = 0; i< 5; i++){
-            playersData.add(rnd.nextInt(100) + 1);
-        }
-
-        return playersData;
     }
 }
